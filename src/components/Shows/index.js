457947f2f482205da13cf4dea09129e216fd5789
momes <mo.enokida@gmail.com>
2023-07-08 16:54:29 -0700

    added show

--- src/components/Shows/index.js
+++ src/components/Shows/index.js
@@ -5,46 +5,42 @@ import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 import { useTheme } from '@mui/material/styles';
 import useMediaQuery from '@mui/material/useMediaQuery';
-
+import {SHOWS} from '../../constants';
 
 function Shows() {
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
 
-  const shows = []
   return (
     <div className="Shows" style={{overflowX:"hidden"}}>
       <Navigation activeTab={'shows'}/>
-      {isMobile && (<Typography style={{color: 'yellow', fontWeight: 'bold', marginTop: '5vh'}}>
-        UPCOMING SHOWS
-      </Typography>)}
-      {shows.length === 0 && (<Typography style={{color: 'white', marginTop: '25vh'}}>
+      {SHOWS.filter((s)=> s.metaDate > new Date()).length === 0 && (<Typography style={{color: 'white', marginTop: '25vh'}}>
          NO UPCOMING SHOWS.
       </Typography>)}
       <>
-      {shows.map((show, i) => 
-        (<Box style={{width:isMobile ? "90vw" : "50vw", margin: '5vh auto'}}>
-          <Typography style={{color: 'white', fontWeight: 'bold'}}>
-        {show.date}            
-        </Typography>
-        <Typography style={{color: 'white'}}>
-        {show.name}            
-        </Typography>
-        <Typography style={{color: 'white'}}>
-        {show.location}            
-        </Typography>
-        {show.ticketLink ? (<Button href={`${show.ticketLink}`}  target="_blank"  style={{margin:'2vh',color: 'white',padding:'2% 4%',border: '1px white solid', borderRadius: '0px'}}>
-          Tickets
-          </Button>) : (
-            <Button disabled style={{margin:'2vh',color: 'white',padding:'2% 4%',border: '1px white solid', borderRadius: '0px'}}>
-            FREE
-            </Button>
-          )}
-        
-          {shows.length > 1 && i !== shows.length -1 ? (<hr style={{width:'50%'}}/>) : null}
-          </Box>
-          
-        )
+      {SHOWS.filter((s)=> s.metaDate > new Date()).map((show, i) => 
+         (<Box style={{width:isMobile ? "90vw" : "50vw", margin: '5vh auto'}}>
+         <Typography style={{color: 'white', fontWeight: 'bold'}}>
+       {show.date}            
+       </Typography>
+       <Typography style={{color: 'white'}}>
+       {show.name.toUpperCase()}            
+       </Typography>
+       <Typography style={{color: 'white', fontStyle: 'italic'}}>
+       {show.location.toUpperCase()}            
+       </Typography>
+       {show.ticketLink ? (<Button href={`${show.ticketLink}`}  target="_blank"  style={{margin:'2vh',color: 'white',padding:'2% 4%',border: '1px white solid', borderRadius: '0px'}}>
+         Tickets
+         </Button>) : (
+           <Button disabled style={{margin:'2vh',color: 'white',padding:'2% 4%',border: '1px white solid', borderRadius: '0px'}}>
+           FREE
+           </Button>
+         )}
+       
+         {SHOWS.length > 1 && i !== SHOWS.length -1 ? (<hr style={{width:'50%'}}/>) : null}
+         </Box>
+         
+       )
       )}
       </>
     </div>
---
+++ src/constants.js
@@ -0,0 +1,7 @@
+export const SHOWS = [
+  { metaDate: new Date(2023,7,15),
+    date: 'JULY 15 2023',
+    name:'Farm house at Cooks garden',
+    location: 'Venice, CA',
+    ticketLink: `https://www.eventbrite.com/e/farm-house-miami-vice-day-party-tickets-664266407707?aff=Furious`}
+  ]
